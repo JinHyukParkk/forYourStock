85d6f27cebd22f37e72d24afbc1e60e80b8dc7e3
Jinhyuk <pjh08190819@naver.com>
2020-09-03 01:58:42 +0900

    json 데이터 unmarshal

--- controller/start.go
+++ controller/start.go
@@ -1,10 +1,36 @@
 package controller
 
 import (
-	"forYourStock/api"
+	"io/ioutil"
+	"forYourStock/model"
+	"encoding/json"
+	"fmt"
+	// "forYourStock/api"
+	
 )
 
 func Start() {
+	// 주식 종목 번호 파일 가져오기
 	// api.GiveCompanyList()
-	api.GiveList()
+	// 종목 번호 파일 읽기
+	// api.GiveList()
+	szCompanyList := getCompanyList()
+
+	var aCompanyList model.CompanyListStruct
+	if err := json.Unmarshal(szCompanyList, &aCompanyList); err != nil {
+		panic(err)
+	}
+	
+}
+
+func getCompanyList() []byte {
+	dat, err := ioutil.ReadFile("data/companyList.json")
+	check(err)
+	return dat
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
