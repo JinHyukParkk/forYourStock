47e47db79f23af8c4de813db1a6e082af749a281
Jinhyuk <pjh08190819@naver.com>
2020-09-04 02:42:58 +0900

    xml model 생성

--- model/companyInfo.go
+++ model/companyInfo.go
@@ -8,3 +8,17 @@ type CompanyStruct struct {
 	Name	string	`json:"name"`
 	Code	string	`json:"code"`
  }
+
+ type CompanyXmlResult struct {
+	 Result []CompanyXmlList `xml:"result"`
+ }
+
+ type CompanyXmlList struct {
+	List CompanyXml `xml:"list"`
+}
+type CompanyXml struct {
+	corp_code	string	`xml:"corp_code"`
+	corp_name	string	`xml:"corp_name"`
+	stock_code	string	`xml:"stock_code"`
+	modify_date	string	`xml:"modify_date"`
+}
