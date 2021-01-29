53a8650ad32a9e85123e24176e58e6514afed532
hyuk <pjh08190819@naver.com>
2021-01-29 23:18:14 +0900

    crawling test

--- libs/crawling/crwal.go
+++ libs/crawling/crwal.go
@@ -5,18 +5,29 @@ import (
 	"net/http"
 
 	"forYourStock/libs/common"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 //Scrapper function
 func Scrapper(code string) {
-	var baseURL string = "https://finance.naver.com/item/frgn.nhn?code=" + code
+	var baseURL string = "https://finance.naver.com/item/main.nhn?code=" + code
 
+	fmt.Println(baseURL)
 	res, err := http.Get(baseURL)
 	common.CheckErr(err)
 	common.CheckCode(res)
 
 	defer res.Body.Close()
 
-	fmt.Println(res)
+	fmt.Println(res.Body)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	common.CheckErr(err)
+
+	doc.Find(".tah p11 nv01").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Text())
+	})
+
 	fmt.Println("Done")
 }
